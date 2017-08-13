e6dd27127e11d8e742bc89c647e0a7c97d9437b8
Robs68 <robin.irlinger@gmail.com>
2017-08-13 22:28:14 +0000

    multivol

--- parse.py
+++ parse.py
@@ -37,15 +37,39 @@ for i in test :
 	for o in test2:
 		destination = o['destination']
 		origine = o['origin']
-		print("origine :",origine, "destination :", destination)
+#		print("origine :",origine, "destination :", destination)
 		#print o['destination']
 		current_d.append(destination)
 		current_o.append(origine)
 trip = origine_air + destination_air
 #print destination_air
 #print origine_air
-print ("")
+#print ("")
 #pprint(origine_air)
 #pprint(destination_air)
 print trip
 print ("")
+duree_trip = data["trips"]["tripOption"][0]["slice"][0]["duration"]
+duree_trip_h = duree_trip // 60
+print duree_trip_h
+price = data["trips"]["tripOption"][0]["saleTotal"]
+print price
+
+#mutlievols
+multivol = data['trips']['tripOption']
+for p in multivol : 
+	multivol1 = p['slice']
+	for q in multivol1 : 
+		multivol2 = q['segment']
+		durra = q['duration']
+		print durra
+		for s in multivol2 :
+			multivol3 = s['leg']
+			for d in multivol3 : 
+				ori = d['origin']
+				dest = d['destination']
+				vola = ori + dest
+				print vola
+ 
+				
+
