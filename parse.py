9a35cbbc04f1fda582aa5119639dbbeca72cb03e
Robs68 <robin.irlinger@gmail.com>
2017-08-14 13:10:58 +0000

    multiflights

--- parse.py
+++ parse.py
@@ -12,12 +12,8 @@ with open('out2') as data_file:
 
 #pprint(data)
 
-#print("fichier ouvert")
-
 test = data['trips']['tripOption'][0]['slice'][0]['segment']
 #print test
-
-print("")
 
 #for i in data[['trips']['tripOption'][0]['slice'][0]['segment']]:
 #	print i[['leg'][0]['origin']]
@@ -25,29 +21,31 @@ print("")
 #for x in data["trips"]["tripOption"][0]["slice"][0]["segment"][0]["leg"]:
 #	print(x['airport_o'])
 
-origine_air = []
-destination_air = []
-for i in test :
-	current_d = [] 
-	current_o = []
-	destination_air.append(current_d)
-	origine_air.append(current_o)
+#origine_air = []
+#destination_air = []
+#for i in test :
+#	current_d = [] 
+#	current_o = []
+#	destination_air.append(current_d)
+#	origine_air.append(current_o)
 #	print i['leg']
-	test2 = i['leg']
-	for o in test2:
-		destination = o['destination']
-		origine = o['origin']
+#	test2 = i['leg']
+#	for o in test2:
+#		destination = o['destination']
+#		origine = o['origin']
 #		print("origine :",origine, "destination :", destination)
 		#print o['destination']
-		current_d.append(destination)
-		current_o.append(origine)
-trip = origine_air + destination_air
+#		current_d.append(destination)
+#		current_o.append(origine)
+#trip = origine_air + destination_air
+
+#print origine_air[1] afficher le 2eme elements de la liste
 #print destination_air
 #print origine_air
 #print ("")
 #pprint(origine_air)
 #pprint(destination_air)
-print trip
+#print trip
 print ("")
 #duree_trip = data["trips"]["tripOption"][0]["slice"][0]["duration"]
 #duree_trip_h = duree_trip // 60
@@ -57,6 +55,8 @@ print ("")
 
 #mutlievols
 multivol = data['trips']['tripOption']
+origine_air = []
+destination_air = []
 for p in multivol : 
 	print("")
 	multivol1 = p['slice']
@@ -76,6 +76,7 @@ for p in multivol :
 				heure_de = d['departureTime']
 				vol_entier = ori + dest
 				print vol_entier
- 
-				
+				origine_air.append(ori)
 
+print origine_air
+print destination_air
