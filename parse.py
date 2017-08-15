1b3000303cd886f8fe74ea24398d14068790b147
Robs68 <robin.irlinger@gmail.com>
2017-08-15 18:25:37 +0000

    tupple

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from pprint import pprint
+import itertools
 
 print("Lancement du script")
 
@@ -17,17 +18,23 @@ multivol = data['trips']['tripOption']
 origine_air = []
 destination_air = []
 vol_entier = []
-for p in multivol : 
+price = []
+count_vol = 0
+count_vol_connecting = 0
+for p in multivol :
 	print("")
 	multivol1 = p['slice']
 	prix = p['saleTotal']
+	price.append(prix)
 	for q in multivol1 : 
+		count_vol += 1
 		multivol2 = q['segment']
 		duree_trip = q['duration']
 		duree_trip_h = duree_trip // 60
 		for s in multivol2 :
 			multivol3 = s['leg']
 			for d in multivol3 : 
+				count_vol_connecting += 1
 				ori = d['origin']
 				dest = d['destination']
 				heure_ar = d['arrivalTime']
@@ -35,10 +42,12 @@ for p in multivol :
 				vol_entier = ori + dest
 #				print vol_entier
 				origine_air.append(ori)
+				destination_air.append(dest)
 #				print origine_air
 										
-print ("resultat list", origine_air)
 #print destination_air
+print count_vol
+print count_vol_connecting
 titi = []
 for i in range(0,len(origine_air)):
 	origine = origine_air[i][0:3] #trouve les 3 premier caracteres
@@ -46,6 +55,18 @@ for i in range(0,len(origine_air)):
 	titi.append(origine)
 	titi.append(destination)
 print("")
-print titi[0]
-print titi[1]
+#print titi[0]
+#print titi[1]
 
+concatenate = zip(origine_air,destination_air)
+print concatenate
+#sauvegarde des valeurs recues
+vol_1_part_1 = concatenate[0][0]+concatenate[0][1]
+vol_1_part_2 = concatenate[1][0]+concatenate[1][1]
+vol_1 = vol_1_part_1 + vol_1_part_2
+
+vol_2_part_1 = concatenate[2][0]+concatenate[2][1]
+vol_2_part_2 = concatenate[3][0]+concatenate[3][1]
+vol_2 = vol_2_part_1 + vol_2_part_2
+
+print price
