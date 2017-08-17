287b01cf90f906b60825a4c40d4754cf18c426c5
Robs68 <robin.irlinger@gmail.com>
2017-08-17 22:57:22 +0000

    ajout SQL

--- parse.py
+++ parse.py
@@ -2,14 +2,14 @@ import requests
 import json
 from pprint import pprint
 import itertools
-
+import sqlite3
+# coding=utf-8
 print("Lancement du script")
 
 file = open("out2", "r")
 fichier=file.read()
 with open('out2') as data_file:
 	data = json.load(data_file)
-
 
 #test = data['trips']['tripOption'][0]['slice'][0]['segment']
 #mutlievols
@@ -56,17 +56,32 @@ for i in range(0,len(origine_air)):
 print("")
 #print titi[0]
 #print titi[1]
-
+prix_vol_1 = price[0][3:7]
 concatenate = zip(origine_air,destination_air)
-print concatenate
+#print concatenate
 #sauvegarde des valeurs recues
 vol_1_part_1 = concatenate[0][0]+concatenate[0][1]
 vol_1_part_2 = concatenate[1][0]+concatenate[1][1]
 prix_vol_1 = price[0][3:7]
-vol_1 = [vol_1_part_1, vol_1_part_2,prix_vol_1]
-print vol_1
+vol_1 = [vol_1_part_1,vol_1_part_2,prix_vol_1]
+#print vol_1
 vol_2_part_1 = concatenate[2][0]+concatenate[2][1]
 vol_2_part_2 = concatenate[3][0]+concatenate[3][1]
 prix_vol_2 = price[1][3:7]
 vol_2 = [vol_2_part_1,vol_2_part_2,prix_vol_2]
-print vol_2
+#print vol_2
+vol = zip(vol_1,vol_2,price)
+print vol
+
+#enregistrement
+file=open("result_vol_1","w")
+fichier=file.write(str(vol_1))
+file.close()
+
+#test SQL
+connection = sqlite3.connect("travel.db")
+cursor = connection.cursor()
+#var_string = ', '.join(map(str, vol))
+query = 'INSERT INTO orylax VALUES (?,?,?)'
+cursor.executemany(query, vol)
+connection.commit()
