2587bd333af853eddb7848f70a187df9807c727f
Robs68 <robin.irlinger@gmail.com>
2017-09-09 19:08:34 +0200

    modify

--- parse.py
+++ parse.py
@@ -5,9 +5,9 @@ import itertools
 import sqlite3
 print("Lancement du script")
 
-file = open("out2", "r")
+file = open("out3", "r")
 fichier=file.read()
-with open('out2') as data_file:
+with open('out3') as data_file:
 	data = json.load(data_file)
 
 #mutlievols
@@ -16,6 +16,7 @@ origine_air = []
 destination_air = []
 vol_entier = []
 price = []
+vol_entier2 = []
 count_vol = 0
 count_vol_connecting = 0
 for p in multivol :
@@ -36,24 +37,17 @@ for p in multivol :
 				dest = d['destination']
 				heure_ar = d['arrivalTime']
 				heure_de = d['departureTime']
-				vol_entier = ori + dest
-#				print vol_entier
+				vol_entier = ori + dest + heure_ar + heure_de
+				vol_entier2.append(vol_entier)
+# 				print vol_entier
 				origine_air.append(ori)
 				destination_air.append(dest)
-#				print origine_air
+# 				print origine_air
 										
 #print destination_air
-print count_vol
-print count_vol_connecting
-titi = []
-for i in range(0,len(origine_air)):
-	origine = origine_air[i][0:3] #trouve les 3 premier caracteres
-	destination = origine_air[i][3:6]
-	titi.append(origine)
-	titi.append(destination)
-print("")
-#print titi[0]
-#print titi[1]
+#print count_vol
+#print count_vol_connecting
+#print vol_entier2
 prix_vol_1 = price[0][3:7]
 concatenate = zip(origine_air,destination_air)
 #print concatenate
@@ -62,13 +56,25 @@ vol_1_part_1 = concatenate[0][0]+concatenate[0][1]
 vol_1_part_2 = concatenate[1][0]+concatenate[1][1]
 prix_vol_1 = price[0][3:7]
 vol_1 = [vol_1_part_1,vol_1_part_2,prix_vol_1]
-print vol_1
+#print vol_1
 vol_2_part_1 = concatenate[2][0]+concatenate[2][1]
 vol_2_part_2 = concatenate[3][0]+concatenate[3][1]
+vol_2_part_3 = concatenate[4][0]+concatenate[4][1]
 prix_vol_2 = price[1][3:7]
-vol_2 = [vol_2_part_1,vol_2_part_2,prix_vol_2]
-print vol_2
-vol = [(vol_1),(vol_2)]
+if "LAX" in vol_2_part_3:
+	vol_2 = [vol_2_part_1,vol_2_part_2,vol_2_part_3,prix_vol_2]
+else:
+	vol_2 = [vol_2_part_1,vol_2_part_2,prix_vol_2]
+#vol3
+vol_3_part_1 = concatenate[5][0]+concatenate[5][1]
+vol_3_part_2 = concatenate[6][0]+concatenate[6][1]
+vol_3_part_3 = concatenate[7][0]+concatenate[7][1]
+prix_vol_3 = price[1][3:7]
+if "LAX" in vol_3_part_3:
+        vol_3 = [vol_3_part_1,vol_3_part_2,vol_3_part_3,prix_vol_3]
+else:
+        vol_3 = [vol_3_part_1,vol_3_part_2,prix_vol_3]
+vol = [(vol_1),(vol_2),(vol_3)]
 #vol = zip(vol_1,vol_2,price)
 print vol
 
@@ -78,9 +84,9 @@ fichier=file.write(str(vol_1))
 file.close()
 
 #test SQL
-connection = sqlite3.connect("travel.db")
-cursor = connection.cursor()
+#connection = sqlite3.connect("travel.db")
+#cursor = connection.cursor()
 #var_string = ', '.join(map(str, vol))
-query = 'INSERT INTO orylax VALUES (?,?,?)'
-cursor.executemany(query, vol)
-connection.commit()
+#query = 'INSERT INTO orylax VALUES (?,?,?)'
+#cursor.executemany(query, vol)
+#connection.commit()
