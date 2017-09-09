b934a2567c8d041ec6b422377112fcc489d2a7a4
Robs68 <robin.irlinger@gmail.com>
2017-09-09 22:36:12 +0200

    fix sql

--- parse.py
+++ parse.py
@@ -20,7 +20,7 @@ vol_entier2 = []
 count_vol = 0
 count_vol_connecting = 0
 for p in multivol :
-	print("")
+#	print("")
 	multivol1 = p['slice']
 	prix = p['saleTotal']
 	price.append(prix)
@@ -51,18 +51,31 @@ for p in multivol :
 prix_vol_1 = price[0][3:7]
 concatenate = zip(origine_air,destination_air)
 #print concatenate
-#sauvegarde des valeurs recues
+
+#initialisation bbd
+connection = sqlite3.connect("travel.db")
+cursor = connection.cursor()
+
+#Tuple des valeurs recues
+#vol1
 vol_1_part_1 = concatenate[0][0]+concatenate[0][1]
 vol_1_part_2 = concatenate[1][0]+concatenate[1][1]
 prix_vol_1 = price[0][3:7]
-vol_1 = [vol_1_part_1,vol_1_part_2,prix_vol_1]
-#print vol_1
+vol_1 = [vol_1_part_1,vol_1_part_2,"",prix_vol_1]
+trajet1=''.join(vol_1_part_1)
+trajet2=''.join(vol_1_part_2)
+trajet3=""
+prix=''.join(prix_vol_1)
+print vol_1
+cursor.executemany('INSERT INTO orylax (trajet1, trajet2, trajet3, prix) values (?,?,?,?)',(vol_1,))
+#vol2
 vol_2_part_1 = concatenate[2][0]+concatenate[2][1]
 vol_2_part_2 = concatenate[3][0]+concatenate[3][1]
 vol_2_part_3 = concatenate[4][0]+concatenate[4][1]
 prix_vol_2 = price[1][3:7]
 if "LAX" in vol_2_part_3:
 	vol_2 = [vol_2_part_1,vol_2_part_2,vol_2_part_3,prix_vol_2]
+	cursor.executemany('INSERT INTO orylax (trajet1, trajet2, trajet3, prix) values (?,?,?,?)',(vol_2,))
 else:
 	vol_2 = [vol_2_part_1,vol_2_part_2,prix_vol_2]
 #vol3
@@ -74,19 +87,21 @@ if "LAX" in vol_3_part_3:
         vol_3 = [vol_3_part_1,vol_3_part_2,vol_3_part_3,prix_vol_3]
 else:
         vol_3 = [vol_3_part_1,vol_3_part_2,prix_vol_3]
+
 vol = [(vol_1),(vol_2),(vol_3)]
 #vol = zip(vol_1,vol_2,price)
 print vol
 
 #enregistrement
-file=open("result_vol_1","w")
-fichier=file.write(str(vol_1))
-file.close()
+#file=open("result_vol_1","w")
+#fichier=file.write(str(vol_1))
+#file.close()
 
 #test SQL
 #connection = sqlite3.connect("travel.db")
 #cursor = connection.cursor()
-#var_string = ', '.join(map(str, vol))
-#query = 'INSERT INTO orylax VALUES (?,?,?)'
-#cursor.executemany(query, vol)
-#connection.commit()
+#var_string = ', '.join(map(str, vol_3))
+#query = 'INSERT INTO orylax VALUES (?,?,?,?)'
+#cursor.executemany(query, vol_3)
+#cursor.executemany('INSERT INTO orylax (trajet1, trajet2, trajet3, prix) values (?,?,?,?)',(vol,))
+connection.commit()
